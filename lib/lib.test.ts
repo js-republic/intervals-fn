0636891c347b2246d2e4150a38448b80b9a03246
Thibault <PBM42@users.noreply.github.com>
2017-11-21 13:15:38 +0100

    feat(intersect): add types signature
    allow passing array instead of simple interval, for both parameters

--- lib/lib.test.ts
+++ lib/lib.test.ts
@@ -1,12 +1,80 @@
 import test from 'ava';
 
+import { IntervalFT, IntervalSE } from './data.structures';
 import { intersect } from './lib';
 
-test('will intersect', t => {
-  t.plan(2);
+const prepareInput = (i1: IntervalSE | IntervalSE[], convertFn: (i: IntervalSE) => any) => {
+  if (Array.isArray(i1)) {
+    return i1.map(convertFn);
+  }
+  return convertFn(i1);
+};
+
+const prepareOutput = (i1: IntervalSE | IntervalSE[], convertFn: (i: any) => any) => {
+  if (Array.isArray(i1)) {
+    return i1.map(convertFn);
+  }
+  return convertFn(i1);
+};
+
+const convertFTtoSE = (r: IntervalFT): IntervalSE => ({ start: r.from, end: r.to });
+const convertSEtoFT = (r: IntervalSE): IntervalFT => ({ from: r.start, to: r.end });
+
+const testFn = (
+  i1: IntervalSE | IntervalSE[],
+  i2: IntervalSE | IntervalSE[],
+  fn: (...n: any[]) => any,
+  testOutputFn: (n: any) => void,
+  t: any
+): void => {
+  const res = fn(i1, i2);
+  const res2 = prepareOutput(
+    fn(prepareInput(i1, convertSEtoFT), prepareInput(i2, convertSEtoFT)),
+    convertFTtoSE
+  );
+  testOutputFn(res);
+  testOutputFn(res2);
+  t.throws(fn.bind(null, [{ test: 1 }], { test: 1 }), 'Unrecognized interval format');
+};
+
+test('will intersect an array with an interval', t => {
+  t.plan(5);
   const r1 = [{ start: 0, end: 5 }, { start: 7, end: 9 }];
   const r2 = { start: 3, end: 8 };
-  const res = intersect(r1, r2);
-  t.true(res[0].start === 3 && res[0].end === 5);
-  t.true(res[1].start === 7 && res[1].end === 8);
+  const testOutputFn = (res: IntervalSE[]): void => {
+    t.true(res[0].start === 3 && res[0].end === 5);
+    t.true(res[1].start === 7 && res[1].end === 8);
+  };
+  testFn(r1, r2, intersect, testOutputFn, t);
+});
+
+test('will intersect two arrays', t => {
+  t.plan(5);
+  const r1 = [{ start: 0, end: 5 }, { start: 7, end: 9 }, { start: 11, end: 15 }];
+  const r2 = [{ start: 3, end: 6 }, { start: 8, end: 10 }, { start: 20, end: 21 }];
+  const testOutputFn = (res: IntervalSE[]): void => {
+    t.true(res[0].start === 3 && res[0].end === 5);
+    t.true(res[1].start === 8 && res[1].end === 9);
+  };
+  testFn(r1, r2, intersect, testOutputFn, t);
+});
+
+test('will intersect two intervals', t => {
+  t.plan(3);
+  const r1 = { start: 0, end: 5 };
+  const r2 = { start: 3, end: 6 };
+  const testOutputFn = (res: IntervalSE): void => {
+    t.true(res.start === 3 && res.end === 5);
+  };
+  testFn(r1, r2, intersect, testOutputFn, t);
+});
+
+test('will intersect an interval and an array', t => {
+  t.plan(3);
+  const r1 = { start: 0, end: 5 };
+  const r2 = [{ start: 1, end: 2 }, { start: 5, end: 10 }];
+  const testOutputFn = (res: IntervalSE): void => {
+    t.true(res.start === 1 && res.end === 2);
+  };
+  testFn(r1, r2, intersect, testOutputFn, t);
 });
--- lib/lib.ts
+++ lib/lib.ts
@@ -1,32 +1,32 @@
+import { drop, dropWhile, isEmpty, unfold } from 'ramda';
+
 import { interval, IntervalFT, IntervalSE } from './data.structures';
 
 const convertFrom = (typeStr: string) => (r: interval): IntervalSE => {
   switch (typeStr) {
-    case 'IntervalSE':
-      return r as IntervalSE;
     case 'IntervalFT':
       return convertFTtoSE(r as IntervalFT);
     default:
       return r as IntervalSE;
   }
 };
+
 const convertFTtoSE = (r: IntervalFT): IntervalSE => ({ start: r.from, end: r.to });
 const convertSEtoFT = (r: IntervalSE): IntervalFT => ({ from: r.start, to: r.end });
 
-const getType = (r: interval): string => {
-  if (r.hasOwnProperty('start')) {
+const getType = (r: interval | interval[]): string => {
+  const inter = Array.isArray(r) ? r[0] : r;
+  if (inter.hasOwnProperty('start')) {
     return 'IntervalSE';
   }
-  if (r.hasOwnProperty('from')) {
+  if (inter.hasOwnProperty('from')) {
     return 'IntervalFT';
   }
-  return '';
+  throw new Error('Unrecognized interval format');
 };
 
-const convertTo = <T extends interval>(typeStr: string, r: IntervalSE): T => {
+const convertTo = <T extends interval>(typeStr: string) => (r: IntervalSE): T => {
   switch (typeStr) {
-    case 'IntervalSE':
-      return r as T;
     case 'IntervalFT':
       return convertSEtoFT(r) as T;
     default:
@@ -34,18 +34,50 @@ const convertTo = <T extends interval>(typeStr: string, r: IntervalSE): T => {
   }
 };
 
+const sortByStart = (i1: IntervalSE, i2: IntervalSE) => i1.start - i2.start;
+
+const prepareInput = (typeStr: string, i: interval | interval[]): IntervalSE[] => {
+  if (Array.isArray(i)) {
+    return i.map(convertFrom(typeStr));
+  }
+  return [convertFrom(typeStr)(i)];
+};
+
+const intersectUnfolder = (
+  [inters1, inters2]: [IntervalSE[], IntervalSE[]]
+): false | [IntervalSE, [IntervalSE[], IntervalSE[]]] => {
+  if (isEmpty(inters1)) {
+    return false;
+  }
+  const inter1 = inters1[0];
+  const newInters2 = dropWhile(i => i.end < inter1.start, inters2);
+  if (isEmpty(newInters2) || newInters2[0].start > inter1.end) {
+    return false;
+  }
+  const inter2 = newInters2[0];
+  return [
+    { end: Math.min(inter1.end, inter2.end), start: Math.max(inter1.start, inter2.start) },
+    [drop(1, inters1), newInters2],
+  ];
+};
+
+const intersectGen = (intervalsA: IntervalSE[], intervalsB: IntervalSE[]): IntervalSE[] => {
+  const intervalsAS = intervalsA.sort(sortByStart);
+  const intervalsBS = intervalsB.sort(sortByStart);
+  return unfold(intersectUnfolder, [intervalsAS, intervalsBS] as [IntervalSE[], IntervalSE[]]);
+};
+
 /**
  *
  * @returns Intersections of `interval1` and `interval2`
  */
-export function intersect<T extends interval>(interval1: T[], interval2: T): T[] {
-  const typeStr = getType(interval2);
-  const r1s = interval1.map(convertFrom(typeStr));
-  const r2 = convertFrom(typeStr)(interval2);
-  return r1s.filter(r1 => r1.start < r2.end && r1.end > r2.start).map(r =>
-    convertTo<T>(typeStr, {
-      end: Math.min(r2.end, r.end),
-      start: Math.max(r2.start, r.start),
-    })
-  );
+export function intersect<T extends interval>(interval1: T, interval2: T): T;
+export function intersect<T extends interval>(interval1: T, interval2: T[]): T[];
+export function intersect<T extends interval>(interval1: T[], interval2: T): T[];
+export function intersect<T extends interval>(interval1: T | T[], interval2: T | T[]): any {
+  const typeStr = getType(interval1);
+  const r1s = prepareInput(typeStr, interval1);
+  const r2s = prepareInput(typeStr, interval2);
+  const res = intersectGen(r1s, r2s).map(convertTo<T>(typeStr));
+  return res.length > 1 ? res : res[0];
 }
