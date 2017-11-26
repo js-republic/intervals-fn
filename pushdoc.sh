709ee966d47ab133ad2eaf88e82ea7929b19df1c
PBM42 <pbm764927@gmail.com>
2017-11-26 14:23:48 +0100

    build(docs): prevent 404 for gh-pages

--- pushdoc.sh
+++ pushdoc.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+touch docs/.nojekyll
+git add docs && git commit -m "docs: update"
 git subtree split --prefix docs -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
