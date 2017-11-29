65b856e98befb69aa4a83b465355f67a3fecca28
PBM42 <pbm764927@gmail.com>
2017-11-29 07:39:14 +0100

    chore(script): update pushdoc to include nojekyll

--- pushdoc.sh
+++ pushdoc.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+npm run doc
 touch docs/.nojekyll
 git add docs && git commit -m "docs: update"
 git subtree split --prefix docs -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
+git reset HEAD~
