b4bc0d5432d164ca081272cd8b4637345889b0bd
PBM42 <pbm764927@gmail.com>
2018-01-08 21:32:52 +0100

    chore: update pushdoc script to force add

--- pushdoc.sh
+++ pushdoc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 npm run doc
 touch docs/.nojekyll
-git add docs && git commit -m "docs: update"
+git add -f docs && git commit -m "docs: update"
 git subtree split --prefix docs -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
